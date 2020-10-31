8b947dc8343b3d0c9439e23846a25c7ece119aad
Pablo Lucero Schneider <pabloluceroschneider@gmail.com>
2020-10-31 01:01:32 -0300

    Agrego pagina items

--- src/routes/items/index.js
+++ src/routes/items/index.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import ContentWrapper from '../../containers/ContentWrapper'
+import React, { useState, useEffect } from 'react';
+import ContentWrapper from '../../containers/ContentWrapper';
+import HttpRequestRepository from '../../utils/HttpRequestRepository'
 
-const Home = () => {
+const Items = () => {
+    const [ results, setResults] = useState();
+	const [ search ] = useState(new URLSearchParams(window.location.search).get('search') || '');
+
+
+    useEffect(()=>{
+        let getData = async () => {
+            let data = await HttpRequestRepository.fetchData("/items", { q: search});
+            setResults(data.results);
+        }
+        getData()
+    },[search])
+
     return (
-        <ContentWrapper topnav breadscrumb/>
+        <ContentWrapper topnav breadscrumb>
+            <div className="product-wrapper">
+                {results?.map( item => {
+                    return (
+                        <div>{item.id}</div>
+                    )
+                })}
+            </div>
+        </ContentWrapper>
     )
 }
 
-export default Home;
+export default Items;
