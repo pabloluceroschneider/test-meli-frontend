d7d6fcc110a1cd78a66f6aad7689dc097a602127
Pablo Lucero Schneider <pabloluceroschneider@gmail.com>
2020-10-31 01:45:16 -0300

    Agrego componente Product

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import toCurrency from '../../utils/toCurrency';
+import ic_shipping from '../../assets/ic_shipping@2x.png.png';
 
 const Product = ({item}) => {
     return (
@@ -7,7 +9,17 @@ const Product = ({item}) => {
             <a href={`items/${item.id}`} className="photo">
                 <img src={item.thumbnail} alt=""></img>
             </a>
-            
+
+            <div className="description">
+                <div className="price-shipping">
+                    <div className="price"> {toCurrency(item.price)} </div>
+                    {item.shipping.free_shipping ? 
+                        <img className="shipping" src={ic_shipping} alt={item.id}></img>
+                    : null}
+                </div>
+                <a className="title" href={`items/${item.id}`}>{item.title}</a>
+            </div>
+
         </div>
     )
 }
