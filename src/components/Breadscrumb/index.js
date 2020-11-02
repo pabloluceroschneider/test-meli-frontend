dce0f9fd20b5459625348229db0dfb56e228559d
Pablo Lucero Schneider <pabloluceroschneider@gmail.com>
2020-11-01 22:31:19 -0300

    Agrego breadcumb

--- src/components/Breadscrumb/index.js
+++ src/components/Breadscrumb/index.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { AppContext } from '../../store';
 
 const Breadscrumb = () => {
+    const { store } = useContext(AppContext);
     return (
         <div className="breadscrumb-wrapper">
-            Breadscrumb
+            {store?.categories?.map( (cat, index) => {
+                return (
+                    <div key={index} className="category">
+                        {cat}
+                    </div>
+                )
+            })}
         </div>
     )
 }
-export default Breadscrumb
+export default Breadscrumb;
